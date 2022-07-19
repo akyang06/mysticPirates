1a97e2a5aaf140bf21ddc2790181b83dee31199f
Alex Savic <alex@Alexs-MacBook-Pro-6.local>
2022-07-19 00:08:14 -0400

    following ships

--- player.cpp
+++ player.cpp
@@ -21,33 +21,13 @@
  * @effects: sets the values of playerWidth and playerHeight, also defines the playerTexture
  * @notes:
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-player::player(int screenWidth, int screenHeight) {
-    this->screenWidth = screenWidth;
-    this->screenHeight = screenHeight;
+player::player(int screenWidth, int screenHeight) : ship(screenWidth, screenHeight) {
 
-    playerWidth = this->screenWidth / 22;
-    playerHeight = this->screenHeight / 11;
-    rotation = 0;
-    velMag = 0.0;
-
-    /* Loads in image and resizes it for texture */
-    Image sprite = LoadImage("./images/pirateShip.png");
-    ImageResize(&sprite, playerWidth, playerHeight);
-    ImageRotateCW(&sprite);
-    playerTexture = LoadTextureFromImage(sprite); 
-    UnloadImage(sprite);
-    playerWidth = playerTexture.width;
-    playerHeight = playerTexture.height;
-
-    /* Source rectangle (part of the texture to use for drawing) */
-    sourceRec = (Rectangle){0.0, 0.0, playerWidth, playerHeight};
-
-    /* Destination rectangle (screen rectangle where drawing part of texture) */
-    destRec = (Rectangle){this->screenWidth / 2, this->screenHeight / 2, playerWidth, playerHeight};
-
-    /* Origin of the texture (rotation/scale point) */
-    origin = (Vector2){playerWidth / 2, playerHeight / 2};
-
+    acceration = 0.015;
+    deceleration = 0.01;
+    turnDrag = 0.005;
+    drag = 0.0015;
+    velLimit = 5;
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -65,29 +45,6 @@ player::~player() {
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * @function: drawPlayer
- * @purpose: Draws player on the screen
- *
- * @parameters: none
- *     
- * @returns: nothing
- * @effects: Updates the destRec x and y components based on velocity and rotation
- * @notes: 
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-void player::drawPlayer() {
-
-    
-    Vector2 velComp = {(float) cosf(rotation) * velMag, (float) sinf(rotation) * velMag};
-    destRec.x += velComp.x;
-    destRec.y += velComp.y;
-
-    /* Draws player on screen
-    Note: rotation is multiplied by FPS to compensate for the BeginDrawing function */
-    DrawTexturePro(playerTexture, sourceRec, destRec, origin, rotation * GetFPS(), (Color){255,255,255,255});
-
-}
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * @function: monitorPlayer
  * @purpose: Monitor the user for any keys being pressed to then move the ship
  *
@@ -98,34 +55,14 @@ void player::drawPlayer() {
  * @notes:   Calls movePlayer() function to move the player
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void player::monitorPlayer() {
-    /* Monitor for buttons pressed */
-    if (IsKeyDown(KEY_UP)) {
-        up = true;
-    } else {
-        up = false;
-    }
-
-    if (IsKeyDown(KEY_LEFT)) {
-        left = true;
-    } else {
-        left = false;
-    }
-
-    if (IsKeyDown(KEY_DOWN) && velMag > 0) {
-        down = true;
-    } else {
-        down = false;
-    }
-
-    if (IsKeyDown(KEY_RIGHT)) {
-        right = true;
-    } else {
-        right = false;
-    }
 
     /* move player and deal with kinematics */
+    rotatePlayer();
     movePlayer();
-    
+
+    DrawText(TextFormat("velocity x: %f", velComp.x), 20, 80, 20, (Color){255,255,255,255});
+    DrawText(TextFormat("velocity y: %f", velComp.y), 20, 110, 20, (Color){255,255,255,255});
+    DrawText(TextFormat("velocity mag: %f", velMag), 20, 140, 20, (Color){255,255,255,255});
 }
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -135,37 +72,49 @@ void player::monitorPlayer() {
  * @parameters: none
  *     
  * @returns: nothing
- * @effects: Player position and orientation changes
+ * @effects: Player position changes
  * @notes:
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 void player::movePlayer(){
-    /* Note for later: if rotation hits a limit, reset */
-    if (right) {
-        rotation += 0.01;
-        if (velMag > 0) {
-            velMag -= 0.01;
-        }
-    }
 
-    if (left) {
-        rotation -= 0.01;
-        if (velMag > 0) {
-            velMag -= 0.01;
-        }
-    }
-
-    if (up) {
-        velMag += 0.015;
+    if (IsKeyDown(KEY_UP)) {
+        accelerateShip(acceration);
     } else if (velMag > 0) {
-        /* drag force */ velMag -= 0.01;
-    
+        /* drag force */
+        decelerateShip(drag);
     }
 
-    if (down) {
-        velMag -= 0.0025;
+    if (IsKeyDown(KEY_DOWN)) {
+        decelerateShip(deceleration);
     }
 
-    if (velMag < 0.0) {
-        velMag = 0.0;
+    if (outOfBounds() && !facingInBounds()) {
+        boundCollision();
     }
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * @function: rotatePlayer
+ * @purpose: Rotate the player on the screen based on arrow key buttons being pressed
+ *
+ * @parameters: none
+ *     
+ * @returns: nothing
+ * @effects: Player orientation changes
+ * @notes:
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+void player::rotatePlayer(){
+    if (IsKeyDown(KEY_RIGHT)) {
+        rotation += 0.01;
+        decelerateShip(turnDrag);
+    }
+
+    if (IsKeyDown(KEY_LEFT)) {
+        rotation -= 0.01;
+        decelerateShip(turnDrag);
+    }
+
+    /* Keeps the rotation between 0 and 2pi radians */
+    rotation = fmod(rotation + (2 * M_PI), 2 * M_PI);
 }
